53431a164ce6f71465e5572d1bf8a591ec692b82
Artur Kazimierski <artur.teliss@gmail.com>
2016-11-20 18:13:17 +0100

    water processing

---
+++ Assets/Scripts/StaticMapDownload.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticMapDownload : MonoBehaviour {
+
+    string startUrl = "https://maps.googleapis.com/maps/api/staticmap";
+    string center = "?center=" + "53.4281462,14.5646163";
+    string zoom = "&zoom=" + "16";
+    string size = "&size=" + "512x512";
+    string format = "&format=" + "png32";
+    string style = "&style=" + "element:labels|visibility:off" + "&style=" + "feature:water|color:0x000000";
+    string key = "&key=AIzaSyBWLfFwdWQUpZn0gJ6ljq67n8Y2A2x6OzE";
+    public GameObject plane;
+
+    // Use this for initialization
+    void Start () {
+        string url = startUrl + center + zoom + size + format + style + key;
+        WWW www = new WWW(url);
+
+        while (!www.isDone) ;
+        Color[] pixels = www.texture.GetPixels();
+
+        Texture2D mapTex = new Texture2D(www.texture.width, www.texture.height);
+
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            if (pixels[i] == Color.black)
+            {
+                pixels[i] = new Color(0, 0, 0, 0);
+            }
+        }
+        mapTex.SetPixels(pixels);
+        mapTex.Apply();
+             
+        var renderer = plane.GetComponent<Renderer>();
+        renderer.material.mainTexture = mapTex;
+        renderer.material.shader = Shader.Find("Unlit/Transparent");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
